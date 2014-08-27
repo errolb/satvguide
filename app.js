c4fd1ae3a1f659ea2a79fda99d4b1a03665cbaec
errolb <errolburger1@gmail.com>
2014-08-27 14:47:52 +0200

    working api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var monk = require('monk');
 var db = monk('localhost:27017/tvguidedb');
+var moment = require('moment');
 
 // Make our db accessible to our router
 app.use(function(req,res,next){
@@ -18,15 +19,34 @@ app.get('/', function (req, res) {
 
 // API ROUTES
 
-// GET
-app.get('/api/tvguide/:arg', function(req, res){
+// all data from a single channel
+app.get('/api/tvguide/channel/:arg', function(req, res){
   var db = req.db;
 
-    // NOTE!!: to add dynamic routing. Can't be dumping the whole collection.
-    var collection = db.get('scrapedData');
-    collection.find({'day': req.params.arg},{},function(e, docs){
-        res.send(docs);
-    });
+  var channel = req.params.arg;
+
+  var collection = db.get('schedules');
+  collection.find({'channel':channel},{},function(e, docs){
+      res.send(docs);
+  });
+});
+
+//all data from a date specific date
+app.get('/api/tvguide/day/:arg', function(req, res){
+  var db = req.db;
+  var day = req.params.arg;
+  var collection = db.get('schedules');
+  // NOTE: Dates are stored in mongodb as UTC 00:00. Queries should role back 
+  // to the preceding day where appropriate, so we have to shift the day over by
+  // two hours.
+  // The week always starts with today.
+  var queryDay = moment().startOf('day').add(day, 'days').subtract(2, 'hours').format()
+  var nextDay = moment(queryDay).add(1, 'days').format() 
+  collection.find({'date':{$gte: new Date(queryDay), $lt: new Date(nextDay)}},{},function(e, docs){
+  //collection.find({'date':{$gte: queryDay, $lt: nextDay}},{},function(e, docs){
+          //console.log(fullQuery);
+      res.send(docs);
+  });
 });
 
 // STATIC PATHS
